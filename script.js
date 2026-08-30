cccbac29e36502e26e7696a525fa464f06994362
Shaba Siddiqui <shabasiddiqui0202@gmail.com>
2026-08-30 15:32:53 +0530

    Refactor mobile menu toggle functionality

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-<script>
-  const menuBtn = document.querySelector(".menu-btn");
-  const mobileMenu = document.querySelector(".mobile-menu");
+const menuBtn = document.querySelector(".menu-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-  menuBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("active");
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
 
-    const icon = menuBtn.querySelector("i");
+  const icon = menuBtn.querySelector("i");
 
-    if (mobileMenu.classList.contains("active")) {
-      icon.className = "ri-close-line";
-    } else {
-      icon.className = "ri-menu-line";
-    }
-  });
-</script>
+  if (mobileMenu.classList.contains("active")) {
+    icon.className = "ri-close-line";
+  } else {
+    icon.className = "ri-menu-line";
+  }
+});
